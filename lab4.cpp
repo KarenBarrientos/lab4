98da64fb235becf2ab93eeeb03956efb5173818c
KarenBarrientos <karenp.bmedina>
2016-08-12 16:16:19 -0600

    ejercicio 2 casi listo

--- lab4.cpp
+++ lab4.cpp
@@ -2,9 +2,123 @@
 
 using namespace std;
 
+int menu();
+int factorial(int numero);
+int formula(int numero,int posicion);
+//void CreandoMatriz (int** m, int numero);
+//void LlenandoMatriz (int** matriz, int numero);
+//void Imprimir (int** m,int numero);
+//void liberar_Memoria(int** m, int numero);
+
 int main(int argc, char const *argv[]){
 	
-	cout<<"Hola :D"<<endl;
+	int opcion=0;
+	
+	while ( (opcion = menu()) != 3){
+
+		if (opcion == 1){
+			int gradoPoli;
+			cout<<"-------------- EJERCICIO 1 --------------"<<endl;
+			//cout<<"Ingrese el grado mas alto del polinomio: "<<endl;
+			//cin>>gradoPoli;
+
+			/*for (int i = 0; i < gradoPoli; ++i){
+				cout<<"Ingrese el polinomio x^"<<i;
+				int polinomio;
+			}*/
+
+			
+ 
+		} // fin if 
+		else{
+			if (opcion==2){
+				cout<<"-------------- EJERCICIO 2 --------------"<<endl;
+				int numero;
+				cout<<"Ingrese la potencia: ";
+				cin>>numero;
+    			int x = numero * 2;
+    			for(int i = 0; i <= numero; ++i,x -= 2){
+        			for(int s = 0; s <= x;s++)
+            			cout<<" ";
+        				for(int k = 0; k <= i;++k){
+            				if(formula(i,k) % 2 == 0){
+                				cout<<setw(8)<<"☻";
+            				}
+            				else{
+                			cout<<setw(8)<<"☺";
+            				}
+
+        				}
+        				cout<<endl;
+    			}
+
+			} // fin opcion2
+		}
+			
+	} //fin while
+
 	
 	return 0;
 }
+
+int menu(){
+	cout << "1. Ejercicio 1" << endl
+	     << "2. Ejercicio 2" << endl
+	     << "3. Salir" << endl;
+	     cout<<"Ingrese su opcion: ";
+	int opcion;
+	do{
+		cin >> opcion;
+		if (opcion != 1 && opcion != 2 && opcion != 3)
+			cout << "Introduzca un valor correcto" << endl;
+	}while (opcion != 1 && opcion != 2 && opcion != 3);
+
+	return opcion;
+}
+
+int factorial(int numero){
+    int factorial = 1;
+    for(int i = 1; i <= numero;++i){
+            factorial *= i;
+    }
+    return factorial;
+}
+
+int formula(int numero,int posicion){
+   int combinacion;
+   combinacion = factorial(numero)/ (factorial(numero-posicion)* factorial(posicion) );
+   
+   return combinacion;  
+}
+
+void CreandoMatriz (int** m, int numero){
+    for (int i =0; i<numero+1;i++){
+        m[i]=new int[numero+1];
+    }
+}
+
+void LlenandoMatriz (int** matriz, int numero){
+    for (int i =0; i<numero+1;i++){
+        for (int j = 0; j < numero+1; ++j){
+            matriz[i][j]=0;
+        }
+    }
+}
+
+void Imprimir (int** m,int numero){
+    for(int i =0; i<numero+1;i++){
+            for(int j =0 ;j<numero+1;j++){
+                cout<<" ["<<m[i][j]<<"]";
+            }
+        cout<<"\n";
+    }   
+}
+
+void liberar_Memoria(int** m, int numero){
+    for(int i =0 ; i <numero+1;i++){
+        delete[] m[i];
+    }
+    delete[] m;
+
+}
+
